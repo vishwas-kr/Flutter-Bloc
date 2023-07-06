85365df837f0fb0a1dce7e1da5ba209c7ae5205b
Vishwas <vishwaskumar445@gmail.com>
2023-07-06 21:58:55 +0530

    setting up

---
+++ lib/data/product_data.dart
@@ -0,0 +1,36 @@
+class ProductData {
+  static List<Map<String, dynamic>> productData = [
+    {
+      'name': 'Product 1',
+      'price': 9.99,
+      'title': 'I am the first product',
+      'id': 1,
+      'image':
+          'https://img.freepik.com/free-vector/realistic-cosmetic-packaging_1268-1266.jpg?size=626&ext=jpg&ga=GA1.1.1399549575.1672332440&semt=sph',
+    },
+    {
+      'name': 'Product 2',
+      'price': 14.99,
+      'title': 'I am the second product',
+      'id': 2,
+      'image':
+          'https://img.freepik.com/free-photo/beauty-products-recipients-assortment-grey-stones_23-2148761390.jpg?size=626&ext=jpg&ga=GA1.1.1399549575.1672332440&semt=sph',
+    },
+    {
+      'name': 'Product 3',
+      'price': 19.99,
+      'title': 'I am the third product',
+      'id': 3,
+      'image':
+          'https://img.freepik.com/premium-photo/makeup-palette-illustration-ai-generativexa_115919-15036.jpg?size=626&ext=jpg&ga=GA1.1.1399549575.1672332440&semt=sph',
+    },
+    {
+      'name': 'Product 4',
+      'price': 24.99,
+      'title': 'I am the fourth product',
+      'id': 4,
+      'image':
+          'https://img.freepik.com/free-photo/beauty-skincare-products-bathroom_53876-133303.jpg?size=626&ext=jpg&ga=GA1.1.1399549575.1672332440&semt=sph',
+    },
+  ];
+}
