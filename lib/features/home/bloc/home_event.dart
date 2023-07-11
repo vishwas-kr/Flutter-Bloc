2d256e4540a26e3bf18f885c7730db2067a0e836
Vishwas <vishwaskumar445@gmail.com>
2023-07-11 22:47:56 +0530

    1

--- lib/features/home/bloc/home_event.dart
+++ lib/features/home/bloc/home_event.dart
@@ -2,3 +2,11 @@ part of 'home_bloc.dart';
 
 @immutable
 abstract class HomeEvent {}
+
+class HomeProductWishlistButtonClickedEvent extends HomeEvent {}
+
+class HomeProductCartButtonClickedEvent extends HomeEvent {}
+
+class HomeWishlistButtonNavigateEvent extends HomeEvent {}
+
+class HomeCartButtonNavigateEvent extends HomeEvent {}
