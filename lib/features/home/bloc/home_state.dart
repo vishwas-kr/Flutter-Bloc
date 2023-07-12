eaf6247e39ff7e5ae13f84b48737cdbdbb22151e
Vishwas <vishwaskumar445@gmail.com>
2023-07-12 12:14:03 +0530

    new codes 😣

--- lib/features/home/bloc/home_state.dart
+++ lib/features/home/bloc/home_state.dart
@@ -3,4 +3,16 @@ part of 'home_bloc.dart';
 @immutable
 abstract class HomeState {}
 
+abstract class HomeActionState extends HomeState {}
+
 class HomeInitial extends HomeState {}
+
+class HomeLoadingState extends HomeState {}
+
+class LoadingSuccessState extends HomeState {}
+
+class HomeErrorState extends HomeState {}
+
+class HomeNaviagteToWishlistPageActionState extends HomeActionState {}
+
+class NavigateToCartPageActionState extends HomeActionState {}
--- lib/features/home/ui/home.dart
+++ lib/features/home/ui/home.dart
@@ -1,14 +1,28 @@
+import 'package:bloc_learn/features/home/bloc/home_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
 
   @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final HomeBloc homeBloc = HomeBloc();
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Product Store'),
-      ),
+    return BlocConsumer<HomeBloc, HomeState>(
+      bloc: homeBloc,
+      listener: (context, state) {},
+      builder: (context, state) {
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text('Product Store'),
+          ),
+        );
+      },
     );
   }
 }
