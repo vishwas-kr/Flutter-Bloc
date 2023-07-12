a9119f0d3d731e4c6d632370a4697e098c13cf02
Vishwas <vishwaskumar445@gmail.com>
2023-07-12 23:09:11 +0530

    .

--- lib/features/home/ui/home.dart
+++ lib/features/home/ui/home.dart
@@ -15,11 +15,20 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return BlocConsumer<HomeBloc, HomeState>(
       bloc: homeBloc,
+      // buildWhen: (previous, current) {},
+      // listenWhen: (previous, current) {},
       listener: (context, state) {},
       builder: (context, state) {
         return Scaffold(
           appBar: AppBar(
             title: const Text('Product Store'),
+            actions: [
+              IconButton(
+                  onPressed: () {}, icon: const Icon(Icons.favorite_border)),
+              IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.shopping_bag_outlined))
+            ],
           ),
         );
       },
