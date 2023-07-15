a1195618fd3972c7da91b34be5b03559c8b6d4bb
Vishwas <vishwaskumar445@gmail.com>
2023-07-15 19:31:11 +0530

    removed linux

--- lib/features/cart/ui/cart_page.dart
+++ lib/features/cart/ui/cart_page.dart
@@ -3,8 +3,6 @@ import 'package:bloc_learn/features/cart/ui/widgets/cart_tile.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import '../../home/ui/widgets/product_tile.dart';
-
 class CartPage extends StatefulWidget {
   const CartPage({super.key});
 
