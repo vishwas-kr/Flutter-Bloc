e8f62dc297286728606b3f0bf38e7cd8da2bc971
Vishwas <vishwaskumar445@gmail.com>
2023-07-15 15:49:48 +0530

    adding more

--- lib/features/home/ui/widgets/product_tile.dart
+++ lib/features/home/ui/widgets/product_tile.dart
@@ -7,6 +7,51 @@ class ProductTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      margin: const EdgeInsets.all(10),
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+          border: Border.all(color: Colors.black, width: 0.5),
+          borderRadius: BorderRadius.circular(15)),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            height: 200,
+            width: double.maxFinite,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: NetworkImage(productDataModel.imageUrl),
+                  fit: BoxFit.cover),
+            ),
+          ),
+          Text(
+            productDataModel.name,
+            style: const TextStyle(fontWeight: FontWeight.bold),
+          ),
+          Text(productDataModel.description),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('\$ ${productDataModel.price}'),
+              Row(
+                children: [
+                  IconButton(
+                      onPressed: () {
+                        // homeBloc.add(HomeWishlistButtonNavigateEvent());
+                      },
+                      icon: const Icon(Icons.favorite_border)),
+                  IconButton(
+                      onPressed: () {
+                        //homeBloc.add(HomeCartButtonNavigateEvent());
+                      },
+                      icon: const Icon(Icons.shopping_bag_outlined))
+                ],
+              )
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
